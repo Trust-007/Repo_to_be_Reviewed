93ea2acd758f3c61aa7fce5c2aaf122a3286c397
Yonas44 <tesfu.yonas1350@gmail.com>
2022-11-10 12:12:41 +0300

    added task method

--- tasks.test.js
+++ tasks.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const { Tasks } = require('./src/modules/task');
+const { Tasks, Task } = require('./src/modules/task');
 
 describe('add & remove', function () {
   it('add task', function () {
@@ -10,19 +10,5 @@ describe('add & remove', function () {
     expect(newTasks.appendTask(test1)).toBe('Wrong input!');
     expect(newTasks.appendTask(test2).length).toBe(1);
     expect(newTasks.appendTask(test3).length).toBe(2);
-  });
-
-  it('remove task', function () {
-    const newTasks = new Tasks();
-    newTasks.appendTask({ first: 1 });
-    newTasks.appendTask({ second: 2 });
-    newTasks.appendTask({ third: 3 });
-    newTasks.appendTask({ fourth: 4 });
-    const test1 = 1;
-    const test2 = 'hello';
-    expect(JSON.stringify(newTasks.taskRemover(test1))).toBe(
-      '[{"first":1},{"third":3},{"fourth":4}]'
-    );
-    expect(newTasks.taskRemover(test2)).toBe('Wrong input!');
   });
 });
