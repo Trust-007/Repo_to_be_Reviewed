06433d781dfb5791a9a74c1a2734823c44b3dc8f
Trust7 <simeontrust7@gmail.com>
2022-11-10 10:43:41 +0100

    add code to test remove task

--- tasks.test.js
+++ tasks.test.js
@@ -11,4 +11,17 @@ describe('add & remove', function () {
     expect(newTasks.appendTask(test2).length).toBe(1);
     expect(newTasks.appendTask(test3).length).toBe(2);
   });
+  it('remove task', function () {
+    const newTasks = new Tasks();
+    newTasks.appendTask(new Task('first'));
+    newTasks.appendTask(new Task('second'));
+    newTasks.appendTask(new Task('third'));
+    newTasks.appendTask(new Task('fourth'));
+    const test1 = 1;
+    const test2 = 'hello';
+    expect(JSON.stringify(newTasks.taskRemover(test1))).toBe(
+      '[{"string":"first","bool":false,"index":null},{"string":"third","bool":false,"index":null},{"string":"fourth","bool":false,"index":null}]'
+    );
+    expect(newTasks.taskRemover(test2)).toBe('Wrong input!');
+  });
 });
